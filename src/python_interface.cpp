2172a18d5cae8da2d91ad0626331e1f847eae4e3
Eric Johnston <ej@machinelevel.com>
2017-07-12 06:47:16 +0100

    [python interface] Add matrix arg-passing, and a quick test to verify the memory is share between Python and C++, and that we can write to the result matrix.

--- src/python_interface.cpp
+++ src/python_interface.cpp
@@ -57,6 +57,47 @@ static PyObject* SimpleTest(PyObject *self, PyObject *args)
 static PyObject* ExpmSpecial(PyObject *self, PyObject *args)
 {
     std::string result_str;
+    float precision = 0.0f;
+    PyArrayObject* src_matrix;
+    PyArrayObject* dst_matrix;
+
+    if (!PyArg_ParseTuple(args, "O!O!f", &PyArray_Type, &src_matrix, &PyArray_Type, &dst_matrix, &precision))
+    {
+        fprintf(stderr, "Error: expm_special() arguments don't match, at %s %s:%d\n", __FUNCTION__, __FILE__, __LINE__);
+        return NULL;
+    }
+
+    size_t src_rows = (size_t)PyArray_DIM(src_matrix, 0);
+    size_t src_cols = (size_t)PyArray_DIM(src_matrix, 1);
+    size_t dst_rows = (size_t)PyArray_DIM(dst_matrix, 0);
+    size_t dst_cols = (size_t)PyArray_DIM(dst_matrix, 1);
+    size_t src_stride = PyArray_STRIDES(src_matrix)[0];
+    size_t dst_stride = PyArray_STRIDES(dst_matrix)[0];
+    bool   src_is_complex = PyArray_ISCOMPLEX(src_matrix);
+    bool   dst_is_complex = PyArray_ISCOMPLEX(dst_matrix);
+    const complex_t* src_ptr = (const complex_t*)PyArray_DATA(src_matrix);
+    complex_t* dst_ptr = (complex_t*)PyArray_DATA(dst_matrix);
+    const char* error_str = NULL;
+
+    // Check the matrces
+    if (src_rows != src_cols || dst_rows != dst_cols)
+        error_str = "Src and dest matrices must be square";
+    if (src_rows != dst_rows || src_cols != dst_cols)
+        error_str = "Src and dest matrices must be the same size";
+    if (!src_is_complex || !dst_is_complex)
+        error_str = "Src and dest matrices must be complex double-precision float";
+    if (src_stride != src_cols * sizeof(complex_t) || dst_stride != dst_cols * sizeof(complex_t))
+        error_str = "Unexpected stride length; matrices may not be packed complex double";
+    if (error_str)
+    {
+        fprintf(stderr, "Error: %s, at %s %s:%d\n", error_str, __FUNCTION__, __FILE__, __LINE__);
+        return NULL;
+    }
+
+
+    for (size_t i = 0; i < dst_rows * dst_cols; ++i)
+        dst_ptr[i] = to_complex((double)i, 0.1);
+
 
     result_str = "ok";
     return Py_BuildValue("s", result_str.c_str());
