c4de8c9470f47546774bc71adc4ed8119716d5df
Brian <flynnbr@tcd.ie>
2017-07-18 11:22:03 +0100

    Changed axis titles in plots. Other minor semantic changes.

--- test_matrices/compare_linalg.py
+++ test_matrices/compare_linalg.py
@@ -7,7 +7,8 @@ import random_matrix as rm
 import matplotlib.pyplot as plt
 
 # matrix_size: how many qubits. matrix will be square of 2^size X 2^size
-matrix_sizes = [1,2,3]
+#matrix_sizes = [1,2,3]
+matrix_sizes = range(1,5)
 track_times=np.zeros([len(matrix_sizes), 4])
 
 
@@ -26,7 +27,7 @@ for i in range(0,len(matrix_sizes)):
 	time_linalg = b-a
 	time_expm = d-c
 
-	track_times[i,0]= matrix_sizes[i]
+	track_times[i,0]= 2**matrix_sizes[i]
 	track_times[i,1]= time_linalg
 	track_times[i,2]= time_expm
 	track_times[i,3]= time_linalg/time_expm
@@ -35,12 +36,16 @@ for i in range(0,len(matrix_sizes)):
 plt.semilogx(basex=2)
 plt.semilogy(track_times[:,0], track_times[:,1], label='LinAlg')
 plt.semilogy(track_times[:,0], track_times[:,2], label='ExpmSpecial')
+plt.xlabel('Square matrix size')
+plt.ylabel('Time (seconds)')
 plt.legend()
 plt.savefig('times.png')
 
 plt.clf()
 plt.semilogx(basex=2)
 plt.plot(track_times[:,0], track_times[:,3], label='Ratio improvement using ExpmSpecial')
-plt.axhline(y=1, color='black', label='Improvement')
+plt.axhline(y=1, color='black', label='Improvement/Loss Cutoff')
+plt.xlabel('Square matrix size')
+plt.ylabel('Improvement')
 plt.legend()
 plt.savefig('improvement.png')
