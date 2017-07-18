bcc213adf6b3e79c7ba180530aaf8bbe1bc08c3b
Brian <flynnbr@tcd.ie>
2017-07-18 11:12:18 +0100

    Added plotting functionality to compare_linalg.py and develop_plots notebook. Plots showing times using linalg and expSpecial are saved. Also a plot showing the ratio of improvement achieved using ExpmSpecial.

--- test_matrices/compare_linalg.py
+++ test_matrices/compare_linalg.py
@@ -7,53 +7,40 @@ import random_matrix as rm
 import matplotlib.pyplot as plt
 
 # matrix_size: how many qubits. matrix will be square of 2^size X 2^size
-do_loop=1 ## turn off to examine calls as below (under else: )
+matrix_sizes = [1,2,3]
+track_times=np.zeros([len(matrix_sizes), 4])
 
-if do_loop==1:
-	matrix_sizes = [1,2,3]
-	track_times=np.zeros([len(matrix_sizes), 4])
+
+for i in range(0,len(matrix_sizes)):
+	size=matrix_sizes[i]
+	mat= rm.get_matrix(size)
 	
-	for i in range(0,len(matrix_sizes)):
-		size=matrix_sizes[i]
-		mat= rm.get_matrix(size)
-		
-		a = time.time()
-		linalg.expm(mat)
-		b = time.time()
+	a = time.time()
+	linalg.expm(mat)
+	b = time.time()
 
-		c=time.time()
-		emat = mu.expm_special(mat)
-		d=time.time()
+	c=time.time()
+	emat = mu.expm_special(mat)
+	d=time.time()
 
-		time_linalg = b-a
-		time_expm = d-c
+	time_linalg = b-a
+	time_expm = d-c
 
-		track_times[i,0]= matrix_sizes[i]
-		track_times[i,1]= time_linalg
-		track_times[i,2]= time_expm
-		track_times[i,3]= time_linalg/time_expm
+	track_times[i,0]= matrix_sizes[i]
+	track_times[i,1]= time_linalg
+	track_times[i,2]= time_expm
+	track_times[i,3]= time_linalg/time_expm
 
-	print('tracking:')
-	print(track_times)
-	
-	
-else: 	
-	#mat = np.identity(matrix_size, dtype=np.complex128)
-	matrix_size = 1
-	test_matrix = rm.get_matrix(matrix_size)
 
-	print('Matrix of size 2^', matrix_size )
-	print('original matrix:')
-	print(test_matrix)
+plt.semilogx(basex=2)
+plt.semilogy(track_times[:,0], track_times[:,1], label='LinAlg')
+plt.semilogy(track_times[:,0], track_times[:,2], label='ExpmSpecial')
+plt.legend()
+plt.savefig('times.png')
 
-	lin_mat = linalg.expm(test_matrix)
-	exp_mat = mu.expm_special(test_matrix)
-
-	diff = lin_mat - exp_mat
-
-	print('linalg:')
-	print(lin_mat)
-	print('expm:')
-	print(exp_mat)
-	print('difference between linalg and expm fnc:')
-	print(diff)
+plt.clf()
+plt.semilogx(basex=2)
+plt.plot(track_times[:,0], track_times[:,3], label='Ratio improvement using ExpmSpecial')
+plt.axhline(y=1, color='black', label='Improvement')
+plt.legend()
+plt.savefig('improvement.png')
