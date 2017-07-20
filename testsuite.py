2e02c2377e0c703950c92c1254f538f8ae7ab925
Eric Johnston <ej@machinelevel.com>
2017-07-20 09:01:59 +0100

    [testsuite] Add timing to testsuite

--- testsuite.py
+++ testsuite.py
@@ -1,10 +1,16 @@
 import matrix_utils as mu
 import numpy as np
+import time
 
 #mu.test_run()
 
-mat = np.identity(8, dtype=np.complex128)
+mat_size = 512
+mat = np.identity(mat_size, dtype=np.complex128)
+
+t_start = time.time()
 emat = mu.expm_special(mat)
+t_end = time.time()
 
 print emat
 
+print 'finished {}x{} in {} seconds.'.format(mat_size, mat_size, t_end - t_start)
