b78c49b080d9448ff47105a564c38279e2f036f8
Brian <flynnbr@tcd.ie>
2017-08-02 14:32:28 +0100

    Added call to compress method within expm_special function call. Exponentiation seems to work up to precision 1e-12, which is the precision of numpy complex data types.

--- test_matrices/validate_expm_special.py
+++ test_matrices/validate_expm_special.py
@@ -15,8 +15,8 @@ def check_correctness_expm_special(num_qubits=1, print_orig_matrix = False, prin
 	size=num_qubits
 	mtx= rm.get_matrix(size)
 
-	# linalg_output=scipy.linalg.expm(mtx)
-	# sparse_linalg_output = scipy.sparse.csc_matrix(linalg_output)
+	linalg_output=scipy.linalg.expm(mtx)
+	sparse_linalg_output = scipy.sparse.csc_matrix(linalg_output)
 
 	sparse_mtx = scipy.sparse.csc_matrix(mtx)
 	scipy_output = scipy.sparse.linalg.expm(sparse_mtx)
@@ -52,16 +52,45 @@ def check_correctness_expm_special(num_qubits=1, print_orig_matrix = False, prin
 	if print_difference is True: 
 		diff_mtx = expm_special_output - linalg_output
 		print diff_mtx
+		
 
-	if np.all(expm_special_output - scipy_output < 1e-15):
+	precision = 1e-15
+	flag = 0 
+	max_diff = 0
+	"""
+	print "Linalg : "
+	print linalg_output
+	print "Expm: "
+	print expm_special_output
+	"""
+	rows = np.shape(mtx)[0]
+	cols = np.shape(mtx)[1]
+	for i in range(rows):
+		for j in range(cols):
+			diff = np.abs(expm_special_output[i][j] - linalg_output[i][j])
+			if diff > max_diff:
+				max_diff = diff
+
+	return max_diff
+	
+"""
+	if np.all(np.abs(np.real(expm_special_output) - np.real(scipy_output)) < 1e-16):
 		#print 'Correct: Expm gives identical output to linalg.'
 		return 0
 	else: 
+		diff_mtx = expm_special_output - linalg_output
+		print diff_mtx
 		#print 'Incorrect: Expm does not give same output as linalg.'
 		return 1
-	
+"""	
+
 check_sum =0
-for q in range(1, 5):
-	for i in range(1, 5): 
-		check_sum += check_correctness_expm_special(num_qubits=q)
-print 'Check sum = ', check_sum
+max_diff_all=0
+for q in range(1, 8):
+	for i in range(1, 10): 
+		current = check_correctness_expm_special(num_qubits=q)
+		if current > max_diff_all:
+			max_diff_all = current
+print "Largest difference: ", max_diff_all 
+#			check_sum += check_correctness_expm_special(num_qubits=q)
+# print 'Check sum = ', check_sum
