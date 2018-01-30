fce003d696d43fa5325e0804c7d5d57c635a5f18
Brian Flynn <flynnbr@tcd.ie>
2018-01-30 15:26:32 +0000

    Memory leaks fixed. Full functionality should now be available.

--- memory_tests/exp_test.py
+++ memory_tests/exp_test.py
@@ -4,6 +4,6 @@ sparse = True
 ham = h.random_hamiltonian(8)
 t=1.4
 
-h.exp_ham(ham, t, enable_sparse_functionality = sparse)
+#h.exp_ham(ham, t, enable_sparse_functionality = sparse)
 
-#h.test_exp_ham_function(max_qubit=7, test_sparse_speedup=True, num_tests = 1, threshold=1e-15)
+h.test_exp_ham_function(max_qubit=7, test_sparse_speedup=True, num_tests = 1, threshold=1e-15)
